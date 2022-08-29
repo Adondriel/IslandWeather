99ec809a9c886b4a1a5e1a958a1af2c3ee8c49e0
Adondriel <adondrielcraft@gmail.com>
2022-08-29 18:37:25 -0400

    updated island weather util

---
+++ util.js
@@ -0,0 +1,62 @@
+var WeatherFinder = {
+
+getWeather(timeMillis, zone) {
+    return this.weatherChances[zone](this.calculateForecastTarget(timeMillis));
+},
+
+calculateForecastTarget: function(timeMillis) { 
+    // Thanks to Rogueadyn's SaintCoinach library for this calculation.
+    // lDate is the current local time.
+
+    var unixSeconds = parseInt(timeMillis / 1000);
+    // Get Eorzea hour for weather start
+    var bell = unixSeconds / 175;
+
+    // Do the magic 'cause for calculations 16:00 is 0, 00:00 is 8 and 08:00 is 16
+    var increment = (bell + 8 - (bell % 8)) % 24;
+
+    // Take Eorzea days since unix epoch
+    var totalDays = unixSeconds / 4200;
+    totalDays = (totalDays << 32) >>> 0; // Convert to uint
+
+    // 0x64 = 100
+    var calcBase = totalDays * 100 + increment;
+
+    // 0xB = 11
+    var step1 = ((calcBase << 11) ^ calcBase) >>> 0;
+    var step2 = ((step1 >>> 8) ^ step1) >>> 0;
+
+    // 0x64 = 100
+    return step2 % 100;
+},
+
+getEorzeaHour: function(timeMillis) {
+    var unixSeconds = parseInt(timeMillis / 1000);
+    // Get Eorzea hour
+    var bell = (unixSeconds / 175) % 24;
+    return Math.floor(bell);
+},
+
+getWeatherTimeFloor: function(date) {
+    var unixSeconds = parseInt(date.getTime() / 1000);
+    // Get Eorzea hour for weather start
+    var bell = (unixSeconds / 175) % 24;
+    var startBell = bell - (bell % 8);
+    var startUnixSeconds = unixSeconds - (175 * (bell - startBell));
+    return new Date(startUnixSeconds * 1000);
+},
+
+weatherChances: {
+"Island Sanctuary": function(chance) {
+  if ((chance -= 25) < 0) { return "Clear Skies"; } 
+  else if ((chance -= 45) < 0) { return "Fair Skies"; } 
+  else if ((chance -= 10) < 0) { return "Clouds"; } 
+  else if ((chance -= 10) < 0) { return "Rain"; } 
+  else if ((chance -= 5) < 0) { return "Fog"; } 
+  else { return "Showers"; } }
+},
+
+weatherLists: {
+"Island Sanctuary": ["Clear Skies", "Fair Skies", "Clouds", "Rain", "Fog", "Showers"],
+}
+};
---
+++ weather.js
@@ -0,0 +1,62 @@
+var displayWeather = function () {
+    $("#weatherDiv").text(WeatherFinder.getWeather(new Date(), 'Island Sanctuary'));
+ }
+
+ function findWeather() {
+    $("#weatherDiv").text('');
+    $("#weatherTableHeaderRow ~ tr").remove()
+    var weatherStartTime = WeatherFinder.getWeatherTimeFloor(new Date()).getTime();
+    var weatherStartHour = WeatherFinder.getEorzeaHour(weatherStartTime);
+    var zone = 'Island Sanctuary';
+    var targetWeather = $("#weatherSelect").val();
+    var targetPrevWeather = $("#previousWeatherSelect").val();
+    var tries = 0;
+    var matches = 0;
+    var weather = WeatherFinder.getWeather(weatherStartTime, zone);
+    var prevWeather = WeatherFinder.getWeather(weatherStartTime - 1, zone);
+    while (tries < 1000 && matches < 30) {
+       var weatherMatch = targetWeather == null;
+       var prevWeatherMatch = targetPrevWeather == null;
+       var timeMatch = false;
+       for (var i in targetWeather) {
+          if (targetWeather[i] == "" || targetWeather[i] == weather) {
+             weatherMatch = true;
+             break;
+          }
+       }
+       for (var i in targetPrevWeather) {
+          if (targetPrevWeather[i] == "" || targetPrevWeather[i] == prevWeather) {
+             prevWeatherMatch = true;
+          }
+       }
+       if ($("#timeBox" + weatherStartHour).is(":checked")) {
+          timeMatch = true;
+       }
+       if (weatherMatch && prevWeatherMatch && timeMatch) {
+          var weatherDate = moment(weatherStartTime).format('dddd MMMM Do YYYY, h:mm:ss a');
+          $("#weatherTable").append('<tr><td>' + prevWeather + '</td><td>' + weather + '</td><td>' + weatherStartHour + ':00</td><td>' + weatherDate + '</td><td>' + WeatherFinder.calculateForecastTarget(weatherStartTime) + '</td></tr>');
+          matches++;
+       }
+       weatherStartTime += 8 * 175 * 1000; // Increment by 8 Eorzean hours
+       weatherStartHour = WeatherFinder.getEorzeaHour(weatherStartTime);
+       prevWeather = weather;
+       weather = WeatherFinder.getWeather(weatherStartTime, zone);
+       tries++;
+    }
+    if (matches == 0) {
+       $("#weatherDiv").append("Couldn't find the desired conditions over the next 1000 weather cycles (~16 Earth days).  Make sure you have selected at least one time period.<br/>");
+    }
+ }
+
+ function populateWeather() {
+    var weathers = WeatherFinder.weatherLists['Island Sanctuary'];
+    console.log(weathers);
+    var selects = $("#weatherSelect").add("#previousWeatherSelect");
+    selects.empty();
+    selects.append('<option value="" selected="selected">Any</option>');
+    for (var w in weathers) {
+       selects.append('<option value="' + weathers[w] + '">' + weathers[w] + '</option>');
+    }
+ }
+
+ $(window).load(function () { populateWeather(); });
